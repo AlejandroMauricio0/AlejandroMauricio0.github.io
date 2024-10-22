8c64cb56fa6ba273ea6374b5d73a00499ba5e5e0
Mauricio1932 <201227@ids.upchiapas.edu.mx>
2024-10-21 22:34:46 -0600

    update

--- pages/projects/notes/js/index.js
+++ pages/projects/notes/js/index.js
@@ -1,8 +1,25 @@
 const notasBonitas = [
-    "<a href='https://youtu.be/Fraqb7wyofE?si=yWPexJRnLsuC0aCs'>clickme song</a>",
-    "<a href='https://youtu.be/c5verVk180w?si=uD462Xfh66EkD4LO'>clickme :3</a>",
-    "<a href='https://youtu.be/x-0KoCAV4mc?si=occQa51xWIlZrtOO'>clickme <3 </a>",
-    "<a href='https://youtu.be/xdfik2FRhck?si=xVgvQpdxUvPOX4_7'>clickme :(</a>",
+    "<a href='https://youtu.be/Fraqb7wyofE?si=yWPexJRnLsuC0aCs'>clickme song</a>", //Me gustas
+    "<a href='https://youtu.be/c5verVk180w?si=uD462Xfh66EkD4LO'>clickme :3</a>", //Dulce amor
+    "<a href='https://youtu.be/x-0KoCAV4mc?si=occQa51xWIlZrtOO'>clickme <3 </a>", //Mi persona fav
+    "<a href='https://youtu.be/xdfik2FRhck?si=xVgvQpdxUvPOX4_7'>clickme :(</a>", //Compartir
+    "<a href='https://youtu.be/s6AB1JvPK8Y?si=z7_KkiZBTIULdl7s'>clickme love</a>", //Prision de amor
+    "<a href='https://youtu.be/iI34LKnjGXU?si=zFfnITP4SxNf_7DA'>clickme 🌅</a>", //Si quieres 
+    "<a href='https://youtu.be/4O9jaSyNCgw?si=qg5WbuYVoKbN0Ek6'>clickme 💘</a>", //Pideme la luna
+    "<a href='https://youtu.be/jCVclJhSvcg?si=a-CV2deBvpOQ6FEl'>clickme 🧑‍🤝‍👩</a>", //Te esperare
+    "<a href='https://youtu.be/2oooF0_1390?si=jJVXf5HC8iS8b5ds'>clickme 🖍️</a>", //Todo cambio
+    "<a href='https://youtu.be/8sY9Cf4HNfY?si=0juqD9oIpNKva_R4'>clickme 💗</a>", //Te quiero a ti
+    "<a href='https://youtu.be/6AStdL1OXSA?si=gTfkDhM7ALcFFsOP'>clickme 💛</a>", //yellow
+    "<a href='https://youtu.be/K8fIzBh-_sg?si=uQ8z4pzdiGsAdJkq'>clickme 💭</a>", //Te quiero tanto
+    "<a href='https://youtu.be/T7rRyICsxRI?si=nsBaQtS23LV0ihiD'>clickme 🫂 </a>", //Abrazame
+    // "<a href='https://youtu.be/bWFjrEBGGHI?si=X-Cb8jfMJwNFJ2AQ'>clickme  </a>", //get you the moon
+    "<a href='https://youtu.be/sZpJ_4lj0y8?si=Sz_S1gEk7d9z83Ue'>clickme 🥰</a>", //Me enamoré de alguien que también se enamoró
+    "<a href='https://youtu.be/eobk_drNVyQ?si=L1-tQ0utm86uDotm'>clickme ❤️‍🔥</a>", //por tu amor
+    "<a href='https://youtu.be/rdaO-kuyyn8?si=ROvrzX-GacFHsB_7'>clickme 💝</a>", //Amor plastique
+    "<a href=''>clickme </a>", //   
+
+    // "<a href=''>clickme </a>", //
+
     "Sé el cambio que deseas ver en el mundo.",
     "Te quiero 💞",
     "Eres la niña más bonita que mis ojos han visto",
@@ -68,6 +85,51 @@ const notasBonitas = [
     "Que hermoso es poder tomar su mano 🥹🥹🥹🥹🥹",
     "Quien me viera haciendo un sitio, para mi niña hermosa<33",
     "Eres mi mailob",
+    "Eres la casualida con los ojos más bonitos que ha llegado mi vida:3🥹💞",
+    "Eres lo que yo le pedi a Dios, tener se siente como si me huviese cumplido esa peticion💌💘 ",
+    "Las horas más lindas las paso contigo 💞",
+    "No se olvida a quien se ama, y a ti te estoy amando ❤️🌹",
+    "Me estoy acostumbrando tanto a ti que, si te vas, me dolerá toda una vida. 😔💔",
+    "Enamorado de ti y tú sonrisa:3",
+    "No dejes de sonreir mi niña:3",
+    "Eres lo mejor que ha llegado a mi vida<3",
+    "Te necesito conmigo:(",
+    "Mi amor por ti es infinito<3",
+    "Que niña hermosa tengo a mi lado<3",
+    "Eres mi motivación de todos los días<3",
+    "Enamorado tuyo 💝",
+    "Tan hermosa como una rosa 🌹, tan brillante como una luna 🌙 y tan perfecta como ninguna 😻",
+    "No import cuanta veces nos veamos, siempre sentire que el tiempo contigo no es suficiente, siempre querre más☹️",
+    "Ahora entiendo porque nunca me atreví a nada, con nadie más... siempre te estube esperando ❤️‍🩹 ",
+    "Me gustaria decir que soy feliz con tan poco pero... soy feliz contigo y tu eres bastante 🥹🥹",
+    "Eres mi pequeñita, mi princesita, mi angelita, mi corazoncito, mi cielito, mi mundo entero, mi felicidad, mi estrellita favorita 💗",
+    "Mi niña, solo concentrate en tus metas, tú chico no irá a ningun lado te apoyara y estara estara en cada paso de tú vida 🤍✨❤️",
+    "Pero me quieres mucho, poquito o bastante?🥹",
+    "Solo quiero recordarte que te amo todos los días y que eres lo mas hermos que tengo en mi corazón, eres mi mayor felicidad en es mundo🥹❤️",
+    "Obvio que te voy a presumir, yo no ocultaria lo que me hace feliz🥹✨",
+    "Me gustas mucho 😻💘",
+    "Pues guapo guapo no soy... pero si tengo a la chica más hermosa y esa ere tu mi amor 💘😻",
+    "Haber hablado contigo es, fue y seguira siendo de las mejores cosas que me ha pasado en la vida 💘",
+    "AMO verte ser mi novia, eres mi niña de ojitos bonitos 💘🥹",
+    "Que pase el tiempo y me sigas mirando de la misma manera, eso quiero<3",
+    "Dios tiene a sus favoritos, a ti te hizo perfecta y a mí me permitio estar contigo:´)",
+    "Donde sea, como sea, cuando sea, pero que todo sea contigo<3",
+    "Me niego a que sea solo seas una etapa, yo te quiero a ti el resto de mi vida",
+    "Dato: hoy 21/10/2024 me senti triste, cansado, no quise hacer mis maletas, me sente en mi escritorio y me puse a poner notas para ti<3",
+    "Tu perfume me encanta",
+    "Me gusta pasar tiempo contigo, tambien me gustan tus besos, tus abrazos, tu sonrisa y simplementente me gustas tú, haria cualquier cosa por verte feliz💗",
+    "Para mi no hay nadie mejor que tu, ni ahora, ni mañana, ni nunca. Eres tú o tú",
+    "Tu sonrisa después de besarnos me enamora 💘",
+    "Ya eres mi novia, pero sigues siendo la persona que me gusta, la niña que me pone nervioso y la niña más hermosa e inigualable😻✨",
+    "Tu Eres la razón por la cual creo que vale la pena sentir amor hacia alguien☹️❤️‍🩹🥹",
+    "Y como no quererte si mi corazoncito volvio a sentirse feliz desde que tú llegaste a mi vida<3❤️‍🩹💘",
+    "Podrán haber muchos lugares bonitos, pero sin duda estar contigo es mi favorito🥹",
+    "Que bonito es estar perdidamente enamorado de ti💘💗",
+    "Me estoy enamorando de ti que ya no me veo sin ti:(",
+    "Eres como un solecito ☀️que ilumina mi vida bien bonito, no te vayas nunca de mi vida☹️",
+    "Eres tan perfecta que no necesita maquillaje, ni peinarse para verse hermosa<3 😻💘",
+    "11:11 mi deseo siempre seras tú",
+    "Me declaro fan de tu mirada llena de estrellas 🌃 como las noches de Van Gogh...",
     // "",
 
 ];
